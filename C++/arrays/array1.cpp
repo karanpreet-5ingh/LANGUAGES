edbcf7dc644dcd12b6d0e2198b4aace454070934
karanpreet-singh-iic <karanpreet.singh@iic.ac.in>
2023-02-18 11:14:24 +0530

    17/02/23_array

---
+++ C++/arrays/array1.cpp
@@ -0,0 +1,16 @@
+//array ko countinues location chahie to store the index
+// array me 1st location ka index zero hoga
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int number[15];
+    cout<<"value at 14 index "<<  number[15]<<endl;
+    cout<<endl<<"Everthing is Fine "<<endl <<endl;
+     // intialising an array
+    int  second[3] = {5,7,11};
+    cout<<"value at 2 index "<<  second[2]<<endl;
+
+return 0;
+}
